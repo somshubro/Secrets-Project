3dce5b391240543707f3c3cb4495a820a97df22e
Somshubro Chatterjee <somshubro20@gmail.com>
2021-10-27 07:57:17 +0000

    Enabled functionality for users to submit secrets.

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -53,11 +54,42 @@ app.get("/register", function(req, res){
 });
 
 app.get("/secrets", function(req, res) {
+    User.find({"secret": {$ne: null}}, function(err, foundUsers) {
+        if(err) {
+            console.log(err);
+        } else {
+            if(foundUsers) {
+                res.render("secrets", {usersWithSecrets: foundUsers});
+                console.log(foundUsers);
+            }
+        }
+    });
+});
+
+app.get("/submit", function(req, res) {
     if(req.isAuthenticated()){
-        res.render("secrets");
+        res.render("submit");
     } else {
         res.redirect("/login");
     }
+});
+
+app.post("/submit", function(req, res) {
+
+    const submittedSecret = req.body.secret;
+
+    User.findById(req.user.id, function(err, foundUser) {
+        if(err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function() {
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    });
 });
 
 app.get("/logout", function(req, res) {
